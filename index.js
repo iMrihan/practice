a247f714f0144ecf8f1af3b0be29e62c963c589b
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-05 11:16:28 +0530

    modified

--- index.js
+++ index.js
@@ -668,23 +668,23 @@ myForEach([1, 2, 3, 4, 5], (el, i, array) => {
 
 // Sorting withe the help of insertion approach
 
-let arr = [23, 21, 32, 4, 3, 8, 7];
+// let arr = [23, 21, 32, 4, 3, 8, 7];
 
-function insertionSort(arr) {
-  let current, j;
-  for (let i = 1; i < arr.length; i++) {
-    current = arr[i];
-    j = i - 1;
+// function insertionSort(arr) {
+//   let current, j;
+//   for (let i = 1; i < arr.length; i++) {
+//     current = arr[i];
+//     j = i - 1;
 
-    while (j >= 0 && arr[j] > current) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
+//     while (j >= 0 && arr[j] > current) {
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
 
-    arr[j + 1] = current;
-  }
-}
+//     arr[j + 1] = current;
+//   }
+// }
 
-insertionSort(arr);
+// insertionSort(arr);
 
-console.log(arr);
+// console.log(arr);
