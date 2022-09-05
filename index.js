fabebc7c7fd3738f819940f9099dce5374601fc6
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-05 11:16:03 +0530

    added more questions

--- index.js
+++ index.js
@@ -665,3 +665,26 @@ myForEach([1, 2, 3, 4, 5], (el, i, array) => {
 // }
 
 // console.log(recursiveBinary(data, start, end));
+
+// Sorting withe the help of insertion approach
+
+let arr = [23, 21, 32, 4, 3, 8, 7];
+
+function insertionSort(arr) {
+  let current, j;
+  for (let i = 1; i < arr.length; i++) {
+    current = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = current;
+  }
+}
+
+insertionSort(arr);
+
+console.log(arr);
